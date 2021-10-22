b47bcf7b5c088000f5e3dc3127326fb118157d45
aryan <aryanm1000@gmail.com>
2021-10-22 18:07:43 +0530

    dropdown

--- main.php
+++ main.php
@@ -21,8 +21,8 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
 
 
   <title>EAlearning</title>
-  <link rel="stylesheet" type="text/css" href="http://localhost/E-learning%20project/css/styles.css">
-  <link href="/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="css/styles.css?version=51">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
   <link rel="icon" href="/favicon.ico">
 
   <script type="text/javascript">
@@ -101,25 +101,31 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
       </div>
       </header>                   
                                         <!-- dropdown  -->
-     <!-- <div class="right">
-     <div class="wrapper">
+     <div class="right">
+     <div class="wrapper" >
     <div class="navbar">
       
-      <ul>
-      <li>
-
 <div class="dropdown">
-    <ul>
-      <li><a href="#"><i class="fas fa-user"></i> Profile-<?php echo $_SESSION['email'] ?></a></li>
-      <li><a href="#"><i class="fas fa-sliders-h"></i> Settings</a></li>
-      <li><a href="#"><i class="fas fa-sign-out-alt"></i> Signout</a></li>
+    <ul type="none" >
+
+    <li><a href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+      </svg>               Profile</a></li>
+     
+    <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
+</svg>                     Settings</a></li>
+      
+      <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg></i>                  Signout</a></li>
   </ul>
 </div>
-</li>
-          </ul>
     </div>  
   </div>
-  </div> -->
+  </div>
   
   <!-- END NAVIGATION-->
   <br>
@@ -340,7 +346,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
 // } 
 
 document.querySelector("#profile ul li").addEventListener("click", function () {
-  this.classList.toggle("active");
+  this.classList.toggle("wrapper");
 });
 
 </script>
